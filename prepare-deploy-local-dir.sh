bc1f0ec5e7467b37494bbd752434b02a1673ccc7
Yvan Lussaud <yvan.lussaud@obeo.fr>
2016-04-25 15:39:43 +0200

    Fixed prepare-deploy-local-dir.sh

--- prepare-deploy-local-dir.sh
+++ prepare-deploy-local-dir.sh
@@ -5,7 +5,7 @@ DEPLOY_LOCAL_DIR=$1/repository
 DEPLOY_GPL_LOCAL_DIR=$1/repository_gpl
 echo "Prepare deploy local dir = ${DEPLOY_LOCAL_DIR}"
 # Create deploy local dir
-mkdir $DEPLOY_LOCAL_DIR 
+mkdir -p $DEPLOY_LOCAL_DIR 
 # Copy update-site to deploy local dir
 cp -r $UPDATE_SITE_DIR/* $DEPLOY_LOCAL_DIR
 cp -r $UPDATE_SITE_GPL_DIR/* $DEPLOY_GPL_LOCAL_DIR
