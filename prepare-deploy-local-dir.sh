5fe50736b9849442d82837062243540842f04b25
Yvan Lussaud <yvan.lussaud@obeo.fr>
2016-04-25 16:01:15 +0200

    Fixed local gpl promot dir.

--- prepare-deploy-local-dir.sh
+++ prepare-deploy-local-dir.sh
@@ -6,6 +6,7 @@ DEPLOY_GPL_LOCAL_DIR=$1/repository_gpl
 echo "Prepare deploy local dir = ${DEPLOY_LOCAL_DIR}"
 # Create deploy local dir
 mkdir -p $DEPLOY_LOCAL_DIR 
+mkdir -p $DEPLOY_GPL_LOCAL_DIR 
 # Copy update-site to deploy local dir
 cp -r $UPDATE_SITE_DIR/* $DEPLOY_LOCAL_DIR
 cp -r $UPDATE_SITE_GPL_DIR/* $DEPLOY_GPL_LOCAL_DIR
