1c26edae2f3e4804adf84784d7513f9a3528768e
Yvan Lussaud <yvan.lussaud@obeo.fr>
2016-06-22 13:29:33 +0200

    Added selection support in SemanticView.

--- plugins/eu.modelwriter.semantic.ide.ui/src/eu/modelwriter/semantic/ide/ui/view/SemanticView.java
+++ plugins/eu.modelwriter.semantic.ide.ui/src/eu/modelwriter/semantic/ide/ui/view/SemanticView.java
@@ -11,13 +11,20 @@
  *******************************************************************************/
 package eu.modelwriter.semantic.ide.ui.view;
 
+import eu.modelwriter.semantic.IBase;
 import eu.modelwriter.semantic.SemanticUtils;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.ui.dialogs.FilteredTree;
 import org.eclipse.ui.dialogs.PatternFilter;
 import org.eclipse.ui.part.ViewPart;
@@ -33,6 +40,11 @@ public class SemanticView extends ViewPart {
 	 * The view ID.
 	 */
 	public static final String ID = "eu.modelwriter.semantic.ide.ui.view.SemanticView"; //$NON-NLS-1$
+
+	/**
+	 * The {@link Set} of current selected {@link IBase}.
+	 */
+	private Set<IBase> selectedBases = new LinkedHashSet<IBase>();
 
 	/**
 	 * Constructor.
@@ -56,6 +68,20 @@ public class SemanticView extends ViewPart {
 		semanticBaseTree.getViewer().setContentProvider(new SemanticBaseRegistryContentProvider());
 		semanticBaseTree.getViewer().setLabelProvider(new SemanticBaseLabelProvider());
 		semanticBaseTree.getViewer().setInput(SemanticUtils.getSemanticRegistry());
+		semanticBaseTree.getViewer().getTree().addListener(SWT.Selection, new Listener() {
+
+			public void handleEvent(Event event) {
+				if (event.detail == SWT.CHECK) {
+					if (((TreeItem)event.item).getChecked()) {
+						selectedBases.add((IBase)event.item.getData());
+					} else {
+						selectedBases.remove((IBase)event.item.getData());
+					}
+				}
+			}
+		});
+
+		getSite().setSelectionProvider(semanticBaseTree.getViewer());
 
 		createActions();
 		initializeToolBar();
@@ -86,6 +112,15 @@ public class SemanticView extends ViewPart {
 	@Override
 	public void setFocus() {
 		// Set the focus
+	}
+
+	/**
+	 * Gets the {@link Set} of selected {@link IBase}.
+	 * 
+	 * @return the {@link Set} of selected {@link IBase}
+	 */
+	public Set<IBase> getBase() {
+		return selectedBases;
 	}
 
 }
