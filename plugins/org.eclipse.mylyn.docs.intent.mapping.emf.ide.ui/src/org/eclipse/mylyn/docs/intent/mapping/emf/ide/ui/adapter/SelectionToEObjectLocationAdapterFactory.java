ee43b78048faf1735a17f829dbf1a3f100010108
Yvan Lussaud <yvan.lussaud@obeo.fr>
2016-09-22 10:53:12 +0200

    Added org.eclipse.mylyn.docs.intent.mapping.emf.ide.ui.tests.

--- plugins/org.eclipse.mylyn.docs.intent.mapping.emf.ide.ui/src/org/eclipse/mylyn/docs/intent/mapping/emf/ide/ui/adapter/SelectionToEObjectLocationAdapterFactory.java
+++ plugins/org.eclipse.mylyn.docs.intent.mapping.emf.ide.ui/src/org/eclipse/mylyn/docs/intent/mapping/emf/ide/ui/adapter/SelectionToEObjectLocationAdapterFactory.java
@@ -41,7 +41,7 @@ public class SelectionToEObjectLocationAdapterFactory implements IAdapterFactory
 	 *
 	 * @see org.eclipse.core.runtime.IAdapterFactory#getAdapter(java.lang.Object, java.lang.Class)
 	 */
-	public Object getAdapter(Object adaptableObject, @SuppressWarnings("rawtypes") Class adapterType) {
+	public IEObjectLocation getAdapter(Object adaptableObject, @SuppressWarnings("rawtypes") Class adapterType) {
 		IEObjectLocation res;
 
 		final IBase currentBase = IdeMappingUtils.getCurentBase();
